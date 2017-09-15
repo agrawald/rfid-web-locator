f0a28d2173028def40e7234e8e33d2e5b9ab6187
dagrawal <agrawal.dheeraj.7@gmail.com>
2017-09-15 21:18:06 +1000

    Disabling the street view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,23 +5,13 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://iothub2dbfunctia910.table.core.windows.net/rfidData',
+  baseUrl: 'http://127.0.0.1:10002/iothub2dbfunctia910/rfidData',
   sas: {
+    st: '2017-09-13T11:33:00Z',
+    se: '2017-09-14T11:33:00Z',
+    sp: 'r',
     sv: '2016-05-31',
-    ss: 'bfqt',
-    srt: 'sco',
-    sp: 'rl',
-    st: '2017-09-13T10:07:00Z',
-    se: '2018-09-14T10:07:00Z',
-    sig: '4ThUXHdxIR/8x6WgbP5fPQzAjU4EFT56lXKf1nGXAck='
+    tn: 'rfiddata',
+    sig: 'zXPttgkyvHNxaNwQjXOqsU5t0MagFGKTVRWfT3Jp3dQ='
   }
-  // baseUrl: 'http://127.0.0.1:10002/iothub2dbfunctia910/rfidData',
-  // sas: {
-  //   st: '2017-09-13T11:33:00Z',
-  //   se: '2017-09-14T11:33:00Z',
-  //   sp: 'r',
-  //   sv: '2016-05-31',
-  //   tn: 'rfiddata',
-  //   sig: 'zXPttgkyvHNxaNwQjXOqsU5t0MagFGKTVRWfT3Jp3dQ='
-  // }
 };
